af9d74c4fc700229368c2c6338849e3b3d389702
FTPyreNova024 <jdaniel.mc4@gmail.com>
2025-03-31 12:32:23 -0400

    worked on the first 3 for the final task

--- Week8/CART263-TASK-7-8/js/main.js
+++ Week8/CART263-TASK-7-8/js/main.js
@@ -1,4 +1,27 @@
-window.onload = async function(){
+window.onload = async function () {
     console.log("task 7-8");
+    try {
+        const response = await fetch('iris.json');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+        // Object properties: sepalLength, sepalWidth, petalLength, petalWidth, species
 
+        let possibleColor = ["#5d3fd3", "#a73fd3", "#d33fb5", "#d35d3f", "#d3a73f"];
+        const irisesWithColors = data.map(iris => {
+            return {
+                ...iris,
+                color: possibleColor[Math.floor(Math.random() * possibleColor.length)]
+            };
+        });
+
+        console.log(irisesWithColors);
+
+        const filteredIrises = irisesWithColors.filter(iris => iris.sepalWidth < 4);
+        console.log(filteredIrises);
+    } catch (error) {
+        console.error('Error fetching the data:', error);
+    }
 }
